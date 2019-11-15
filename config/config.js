fbdad2ae30accafef330d62875debc3d875c738d
Paul Lopez <paul.lopez@edgenuity.com>
2019-11-15 15:38:51 -0700

    working version of sequelize to db

--- config/config.js
+++ config/config.js
@@ -7,8 +7,7 @@ module.exports = {
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
     host: process.env.DB_HOSTNAME,
-    "dialect": "mysql",
-    "operatorsAliases": false
+    "dialect": "mysql"
   },
   "test": {
     username: process.env.DB_USERNAME,
--- index.js
+++ index.js
@@ -1,15 +1,24 @@
-const express = require('express')
-const bodyParser = require('body-parser')
+const express = require('express');
+const bodyParser = require('body-parser');
 
-const app = express()
-app.use(bodyParser.json())
+const app = express();
+app.use(bodyParser.json());
+
+const db = require('./models/index');
 
 // API ENDPOINTS
 app.get('/', (req, res) => {
-	res.json("This is the home page")
-})
+    
+    return db.User.findAll()
+    .then((contacts) => res.send(contacts))
+    .catch((err) => {
+        console.log('There was an error with query', JSON.stringify(err))
+        return res.send(err)
+    })
+    // res.json("This is the home page")
+});
 
 const port = 3000
 app.listen(port, () => {
     console.log(`Running on http://localhost:${port}`)
-})
+});
